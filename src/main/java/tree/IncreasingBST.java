d1abaedbfe71d714770a6d84a1331f536eb49174
shiwenjie <shiwenjie@beisencorp.com>
2018-09-22 23:29:48 +0800

    提交

--- src/main/java/tree/IncreasingBST.java
+++ src/main/java/tree/IncreasingBST.java
@@ -1,4 +1,30 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IncreasingBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> vals = new ArrayList();
+        inorder(root, vals);
+        TreeNode ans = new TreeNode(0), cur = ans;
+        for (int v: vals) {
+            cur.right = cur = new TreeNode(v);
+        }
+        return ans.right;
+    }
+
+    public void inorder(TreeNode node, List<Integer> vals) {
+        if (node == null) return;
+        inorder(node.left, vals);
+        vals.add(node.val);
+        inorder(node.right, vals);
+    }
 }
--- src/main/java/tree/Tree2str.java
+++ src/main/java/tree/Tree2str.java
@@ -1,4 +1,36 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tree2str {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public String tree2str(TreeNode t) {
+        if (t == null) return "";
+        StringBuilder sb = new StringBuilder();
+        treeStr(t,sb);
+        return sb.toString();
+    }
+
+    private void treeStr(TreeNode t, StringBuilder list){
+        if (t == null) return;
+        list.append(t.val);
+        if (t.left == null && t.right == null){
+            return;
+        }
+        list.append("(");
+        treeStr(t.left,list);
+        list.append(")");
+        if (t.right != null){
+            list.append("(");
+            treeStr(t.right,list);
+            list.append(")");
+        }
+    }
 }
