3290cf088218aee8059f752e7bf4b8cafe097018
shiwenjie <shiwenjie@chinadaas.com>
2019-12-22 16:32:37 +0800

    ttee

--- src/main/scala/validMountainArray.scala
+++ src/main/scala/validMountainArray.scala
@@ -3,11 +3,11 @@ object validMountainArray {
     var l = 0
     var r = a.length - 1
     while ((l < r) && (a(l) < a(l + 1))) l += 1
-    while ((r > l) && (a(r) < a(r + 1))) r -= 1
+    while ((r > l) && (a(r) < a(r - 1))) r -= 1
     l == r && l != 0 && r != a.length - 1
   }
 
   def main(args: Array[String]): Unit = {
-    println(validMountainArray(Array(2, 1)))
+    println(validMountainArray(Array(0,3,2,1)))
   }
 }
