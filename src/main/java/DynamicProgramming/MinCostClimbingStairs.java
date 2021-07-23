3901f166ff9fb935dbdd2322166e3caa380e04c6
shiwenjie <shiwenjie@xueqiu.com>
2021-07-23 16:37:49 +0800

    新的开始

--- src/main/java/DynamicProgramming/MinCostClimbingStairs.java
+++ src/main/java/DynamicProgramming/MinCostClimbingStairs.java
@@ -2,15 +2,19 @@ package DynamicProgramming;
 
 public class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
-        int len = cost.length;
-        if (len < 2) {
+        int length = cost.length;
+        if (length < 2) {
             return 0;
-        } else if (len == 2) {
+        } else if (length == 2) {
             return Math.min(cost[0], cost[1]);
         }
-        for (int i = 2; i < len; i++) {
+        for (int i = 2; i < cost.length; i++) {
             cost[i] += Math.min(cost[i - 1], cost[i - 2]);
         }
-        return Math.min(cost[len - 1], cost[len - 2]);
+        return Math.min(cost[length - 1], cost[length - 2]);
+    }
+
+    public static void main(String[] args) {
+        new MinCostClimbingStairs().minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
     }
 }
