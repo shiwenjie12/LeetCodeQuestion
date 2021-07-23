f1dd701633aacd20da4b938a32bd3ede3f252b0e
shiwenjie <shiwenjie@xueqiu.com>
2021-07-23 17:44:02 +0800

    新的开始

--- src/main/java/DynamicProgramming/Rob2.java
+++ src/main/java/DynamicProgramming/Rob2.java
@@ -1,24 +1,22 @@
 package DynamicProgramming;
 
-public class Rob {
-
+public class Rob2 {
     public int rob(int[] nums) {
         int len = nums.length;
         if (len == 1) {
             return nums[0];
-        } else if (len == 2) {
-            return Math.max(nums[0],nums[1]);
         }
-        int x = 0, y = 0, z = 0;
-        for (int i = 0; i < len; i++) {
+        return Math.max(robAction(nums, 0, len - 1), robAction(nums, 1, len));
+    }
+
+    int robAction(int[] nums, int start, int end) {
+        int x = 0;
+        int y, z = 0;
+        for (int i = start; i < end; i++) {
             y = z;
             z = Math.max(y, x + nums[i]);
             x = y;
         }
         return z;
-    }
-
-    public static void main(String[] args) {
-        new Rob().rob(new int[]{2,1,1,2});
     }
 }
